209cbcbf565fbe6acd1d248f5bf80d89c907cbeb
Garrett Honke <garretthonke@gmail.com>
2018-06-15 13:21:39 -0400

    test for volumes

--- APEd3mWrapper/wrapper.py
+++ APEd3mWrapper/wrapper.py
@@ -89,7 +89,7 @@ class ape(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
     def __init__(self, *, hyperparams: Hyperparams, volumes: typing.Dict[str,str]=None)-> None:
         super().__init__(hyperparams=hyperparams, volumes=volumes)
 
-        self.volumes = volumes
+        self.embedding = volumes['en_model']
         self._params = {}
 
         print(self.volumes.keys())
@@ -127,7 +127,7 @@ class ape(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
 
         input_df = inputs
         tree = '../ontologies/class-tree_dbpedia_2016-10.json'
-        embedding = self.volumes['en.model']
+        embedding = self.embedding
         row_agg_func = mean_of_rows
         tree_agg_func = np.mean
         source_agg_func = mean_of_rows
