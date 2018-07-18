f3387c8372e1c0cfe1768831f000cf77beadeb60
garrett <garretthonke@gmail.com>
2018-07-18 19:26:07 -0400

    Update wrapper.py

--- APEd3mWrapper/wrapper.py
+++ APEd3mWrapper/wrapper.py
@@ -55,7 +55,7 @@ class ape(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
             },
             {
                 "type": "PIP",
-                "package_uri": "git+https://github.com/NewKnowledge/nk_ape.git@ecaabba5c68f5d8254dee6a5e6ed5e3926a7920e#egg=nk_ape"
+                "package_uri": "git+https://github.com/NewKnowledge/nk_ape.git@414064de018d5cfd6a7dae593ae275aee79c4170#egg=nk_ape"
             },
             {
                 "type": "PIP",
