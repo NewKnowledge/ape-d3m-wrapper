99a6262eb0ec9ef8264609df3bef095118e84188
garrett <garretthonke@gmail.com>
2018-07-18 19:25:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(name='APEd3mWrapper',
       keywords=['d3m_primitive'],
       install_requires=['pandas >= 0.22.0, < 0.23.0',
                         'numpy >= 1.13.3',
-                        'nk_ape >= 1.0.1'],
+                        'nk_ape >= 1.0.2'],
       dependency_links=[
-          "git+https://github.com/NewKnowledge/nk_ape@ecaabba5c68f5d8254dee6a5e6ed5e3926a7920e#egg=nk_ape-1.0.1"
+          "git+https://github.com/NewKnowledge/nk_ape@414064de018d5cfd6a7dae593ae275aee79c4170#egg=nk_ape-1.0.2"
       ],
       entry_points={
           'd3m.primitives': [
